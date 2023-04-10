be19c21b40f22d7a1dbe81fdb985f8c6099ececd
alelorena <alexandre.lorena@gmail.com>
2023-04-10 15:14:27 -0300

    tratamento de exceções

--- Nota.java
+++ Nota.java
@@ -10,21 +10,30 @@ public class Nota {
 
     public static void main(String[] args){
 
-        try (Scanner scan = new Scanner(System.in)) {
-            double nota; 
+        Scanner scan = new Scanner(System.in);
+        double nota;
 
-            System.out.print("Digite uma nota entre 0 e 10: ");
-
-            nota = scan.nextDouble();
-
-            while(nota < 0 || nota > 10) {
-                System.out.println("Nota inválida! Digite uma nota entre 0 e 10: ");
-
+        while(true) {
+            try {
+                System.out.print("Digite uma nota entre 0 e 10: ");
                 nota = scan.nextDouble();
-
+                
+                if(nota >= 0 && nota <= 10) {
+                    break;
+                }
+                else {
+                    System.out.println("Nota inválida! Digite uma nota entre 0 e 10.");
+                }
+            }
+            catch(Exception e) {
+                System.out.println("Entrada inválida! Digite um número.");
+                scan.next();
             }
         }
-        //System.out.println("Teste de push");
-    }
 
+        System.out.println("Nota válida: " + nota);
+
+        scan.close();
+    }
 }
+
