7f53aba3ff2ca7254edabe00e436aa12cd8a63e2
alelorena <alexandre.lorena@gmail.com>
2023-04-10 15:10:27 -0300

    tratamento de exceções

--- ParEImpar.java
+++ ParEImpar.java
@@ -7,33 +7,40 @@ e a quantidade de números ímpares.
 
 public class ParEImpar {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
-        try (Scanner scan = new Scanner(System.in)) {
-            int quantNumeros;
-            int numero;
-            int quantPares = 0, quantImpares = 0;
-
+        int quantNumeros = 0;
+        while (quantNumeros == 0) {
             System.out.println("Digite a quantidade de números: ");
-            quantNumeros = scan.nextInt();
-    
-                System.out.println("Digite " + quantNumeros + " números:");
-
-            int count = 0;
-            do {
-                System.out.println("Número: ");
-                numero = scan.nextInt();
-
-                    if (numero % 2 == 0) quantPares++;
-                    else quantImpares++;
-
-                count++;
-
-            } while(count < quantNumeros);
-
-            System.out.println("Quantidade Par: " + quantPares);
-            System.out.println("Quantidade Ímpar" + quantImpares);
+            try {
+                quantNumeros = Integer.parseInt(scan.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Digite um número válido.");
+            }
         }
 
-    }  
-    
+        System.out.println("Digite " + quantNumeros + " números:");
+        int quantPares = 0, quantImpares = 0;
+
+        for (int i = 1; i <= quantNumeros; i++) {
+            System.out.println("Número " + i + ":");
+            try {
+                int numero = Integer.parseInt(scan.nextLine());
+
+                if (numero % 2 == 0) {
+                    quantPares++;
+                } else {
+                    quantImpares++;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Entrada inválida. Por favor, digite um número inteiro.");
+                i--; // decrementa i para que o número inválido não seja contado
+            }
+        }
+
+        System.out.println("Quantidade de pares: " + quantPares);
+        System.out.println("Quantidade de ímpares: " + quantImpares);
+
+        scan.close();
+    }
 }
