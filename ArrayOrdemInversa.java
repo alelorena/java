a40e44840cca6c362397d43a98692ce4ca1f028c
alelorena <alexandre.lorena@gmail.com>
2023-04-11 12:34:27 -0300

    commit exercício 1 de array

---
+++ ArrayOrdemInversa.java
@@ -0,0 +1,24 @@
+/*
+Crie um vetor de 6 números inteiros e
+mostre-os na ordem inversa.
+ */
+public class ArrayOrdemInversa {
+    public static void main(String[] args) {
+
+        int[] vetor = { -5, -6, 15, 50, 8, 4 }; // posições 0, 1, 2, 3, 4, 5 = 6 elementos
+
+        // System.out.println(vetor.length); // retorna o tamanho do vetor
+
+        int count = 0;
+        while (count < vetor.length) {
+            System.out.print(vetor[count] + " ");
+            count++;
+        }
+        // mostrando na ordem inversa
+        System.out.println("\nvetor: ");
+        for (int i = (vetor.length - 1); i >= 0; i--) {
+            System.out.println(vetor[i] + " ");
+        }
+
+    }
+}
