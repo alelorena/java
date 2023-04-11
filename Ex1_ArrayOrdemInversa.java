05c9fecfaf5ce08d54dd8ebfccb663c6e6df9b03
alelorena <alexandre.lorena@gmail.com>
2023-04-11 12:39:25 -0300

    atualização

--- Ex1_ArrayOrdemInversa.java
+++ Ex1_ArrayOrdemInversa.java
@@ -2,7 +2,7 @@
 Crie um vetor de 6 números inteiros e
 mostre-os na ordem inversa.
  */
-public class ArrayOrdemInversa {
+public class Ex1_ArrayOrdemInversa {
     public static void main(String[] args) {
 
         int[] vetor = { -5, -6, 15, 50, 8, 4 }; // posições 0, 1, 2, 3, 4, 5 = 6 elementos
---
+++ Ex2_ArrayConsoantes.java
@@ -0,0 +1,10 @@
+/* 
+Faça um programa que leia um vetor de 6 caracteres e
+diga quantas consoantes foram lidas.
+Imprima as consoantes.
+ */
+public class Ex2_ArrayConsoantes {
+    public static void main(String[] args) {
+        
+    }
+}
