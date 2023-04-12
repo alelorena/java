cfb9e5f7a5b88c787ce591b7806808644f323aa7
alelorena <alexandre.lorena@gmail.com>
2023-04-11 22:44:58 -0300

    commit

---
+++ pilha/one.digitalinnovation/No.java
@@ -0,0 +1,36 @@
+public class No {
+
+    private int dado;
+    private No refNo = null;
+
+    public No() {
+
+    }
+
+    public No(int dado) {
+        this.dado = dado;
+    }
+
+    public int getDado() {
+        return dado;
+    }
+
+    public void setDado(int dado) {
+        this.dado = dado;
+    }
+
+    public No getrefNo() {
+        return refNo;
+    }
+
+    public void setrefNo(No refNo) {
+        this.refNo = refNo;
+    }
+
+    @Override
+    public String toString() {
+        return "No{" +
+                "dado=" + dado +
+                '}';
+    }
+}
---
+++ pilha/one.digitalinnovation/Pilha.java
@@ -0,0 +1,85 @@
+public class Pilha {
+
+    private No refNoEntradaPilha;
+
+    public Pilha() {
+        this.refNoEntradaPilha = null;
+    }
+
+    public void push(No novoNo) {
+        No refAuxiliar = refNoEntradaPilha;
+        refNoEntradaPilha = novoNo;
+        refNoEntradaPilha.setrefNo(refAuxiliar);
+    }
+
+    public No pop() {
+        if (!this.isEmpty()) {
+            No noPoped = refNoEntradaPilha;
+            refNoEntradaPilha = refNoEntradaPilha.getrefNo();
+            return noPoped;
+        }
+        return null;
+    }
+
+    public No top() {
+        return refNoEntradaPilha;
+    }
+
+    public boolean isEmpty() {
+        return refNoEntradaPilha == null;
+    }
+
+    @Override
+    public String toString() {
+        String stringRetorno = " ---------------\n";
+        stringRetorno += "    Pilha\n";
+        stringRetorno += " ---------------\n";
+
+        No noAuxiliar = refNoEntradaPilha;
+
+        while (true) {
+
+            if (noAuxiliar != null) {
+                stringRetorno += "[No{dado=" + noAuxiliar.getDado() + "}]\n";
+                noAuxiliar = noAuxiliar.getrefNo();
+            } else {
+                break;
+            }
+        }
+        stringRetorno += " ---------------\n";
+        return stringRetorno;
+    }
+
+    public static void main(String[] args) {
+        Pilha minhaPilha = new Pilha();
+        minhaPilha.push(new No(1));
+        minhaPilha.push(new No(2));
+        minhaPilha.push(new No(3));
+    }
+}
+
+class No {
+    private int valor;
+    private No refNo;
+
+    public No(int valor) {
+        this.valor = valor;
+        this.refNo = null;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public No getrefNo() {
+        return refNo;
+    }
+
+    public void setrefNo(No refNo) {
+        this.refNo = refNo;
+    }
+}
