1a391034b1b84f58e69a1390560c98a2c67b30d7
alelorena <alexandre.lorena@gmail.com>
2023-04-15 19:45:48 -0300

    commit Exercício de métodos - Retorno

--- sobrecarga/one.digitalinnovation/Main.java
+++ sobrecarga/one.digitalinnovation/Main.java
@@ -1,3 +1,21 @@
+/* public class Main {
+
+    public static void main(String[] args) {
+
+        // Quadrilátero
+        System.out.println("Exercício quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d, 5d);
+        Quadrilatero.area(7, 8, 9);
+        Quadrilatero.area(5f, 5f);
+
+        // Retornos
+
+        
+    }
+} */
+
+// exemplo para o exercício da Aula 3 de Métodos: Retorno .
 public class Main {
 
     public static void main(String[] args) {
@@ -9,5 +27,17 @@ public class Main {
         Quadrilatero.area(7, 8, 9);
         Quadrilatero.area(5f, 5f);
 
+        // Retornos
+
+        System.out.println("Exercício retornos");
+
+        double areaQuadrado = Quadrilatero.area(3);
+        System.out.println("Área do quadrado:" + areaQuadrado);
+
+        double areaRetangulo = Quadrilatero.area(5, 5);
+        System.out.println("Área do retângulo:" + areaRetangulo);
+
+        double areaTrapezio = Quadrilatero.area(7, 8, 9);
+        System.out.println("Área do trapézio:" + areaTrapezio);
     }
 }
--- sobrecarga/one.digitalinnovation/Quadrilatero.java
+++ sobrecarga/one.digitalinnovation/Quadrilatero.java
@@ -1,9 +1,9 @@
 /**
  * Classe de exemplo para o exercício da Aula 2 de Métodos.
  */
-public class Quadrilatero {
+/* public class Quadrilatero {
 
-    public static void area(double lado) {
+    public static double area(double lado) {
 
         System.out.println("Área do quadrado:" + lado * lado);
     }
@@ -13,14 +13,46 @@ public class Quadrilatero {
         System.out.println("Área do retângulo:" + lado1 * lado2);
     }
 
-    public static void area(double baseMaior, double baseMenor, double altura) {
+    public static double area(double baseMaior, double baseMenor, double altura) {
 
         System.out.println("Área do trapézio:" + ((baseMaior + baseMenor) * altura) / 2);
     }
 
-    public static void area(float diagonal1, float diagonal2) {
+    public static double area(float diagonal1, float diagonal2) {
 
         System.out.println("Área do losango:" + (diagonal1 * diagonal2) / 2);
+    }
+
+} */
+
+/**
+ * Classe de exemplo para o exercício da Aula 3 de Métodos: Retorno.
+ */
+public class Quadrilatero {
+
+    public static double area(double lado) {
+
+        return lado * lado;
+    }
+
+    public static double area(double lado1, double lado2) {
+
+        return lado1 * lado2;
+    }
+
+    public static double area(double baseMaior, double baseMenor, double altura) {
+
+        return ((baseMaior + baseMenor) * altura) / 2;
+    }
+
+    public static void xpto() {
+
+        System.out.println("Antes");
+        return;
+    }
+
+    public static double abc() {
+        return 1.6;
     }
 
 }
