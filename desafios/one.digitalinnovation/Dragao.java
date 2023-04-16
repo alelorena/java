c6c6043898a5baf975fcab778be7e8954f711a1e
alelorena <alexandre.lorena@gmail.com>
2023-04-16 11:51:36 -0300

    commit - desafio: Dragão

---
+++ desafios/one.digitalinnovation/Dragao.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Dragao {
+
+    public static void main(String[] args) {
+
+        int casos, poderDeLuta;
+
+        Scanner ler = new Scanner(System.in);
+        System.out.print("Digite um número inteiro: ");
+        casos = ler.nextInt();
+
+        for (int i = 0; i < casos; i++) {
+            poderDeLuta = ler.nextInt();
+
+            // Verifica se o poder de luta é maior ou igual a 8000
+            if (poderDeLuta > 8000) {
+                System.out.println("Mais de 8000!");
+            } else {
+                System.out.println("Inseto!");
+            }
+        }
+    }
+}
