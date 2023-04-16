a3979f8ec8830948c9f1f692312fd15ba78f1a4b
alelorena <alexandre.lorena@gmail.com>
2023-04-16 12:13:39 -0300

    correção

--- desafios/one.digitalinnovation/Dragao.java
+++ desafios/one.digitalinnovation/Dragao.java
@@ -6,18 +6,19 @@ public class Dragao {
 
         int casos, poderDeLuta;
 
-        Scanner ler = new Scanner(System.in);
-        System.out.print("Digite um número inteiro: ");
-        casos = ler.nextInt();
+        try (Scanner ler = new Scanner(System.in)) {
+            System.out.print("Digite um número inteiro: ");
+            casos = ler.nextInt();
 
-        for (int i = 0; i < casos; i++) {
-            poderDeLuta = ler.nextInt();
+            for (int i = 0; i < casos; i++) {
+                poderDeLuta = ler.nextInt();
 
-            // Verifica se o poder de luta é maior ou igual a 8000
-            if (poderDeLuta > 8000) {
-                System.out.println("Mais de 8000!");
-            } else {
-                System.out.println("Inseto!");
+                // Verifica se o poder de luta é maior ou igual a 8000
+                if (poderDeLuta > 8000) {
+                    System.out.println("Mais de 8000!");
+                } else {
+                    System.out.println("Inseto!");
+                }
             }
         }
     }
